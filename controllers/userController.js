8eea691fc355f9ebfb3b87f5bd94db65d6bda449
JUSTIN_WANG <justin.wang1ab@gmail.com>
2025-01-02 23:56:24 -0500

    no idea why this is wrong

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,7 @@ const loginUser = asyncHandler(async (req, res) => {
     let requireVerification = false;
     const clientIP = req.headers["x-forwarded-for"] || req.clientIp || req.ip?.split(":").at(-1) || req._remoteAddress;
 
-    if (ips.length > 0 && ips.every((ip) => ip !== String(clientIP))) {
+    if (ips.length > 0 && ips.every((ip) => ip && ip !== String(clientIP))) {
       requireVerification = true;
       try {
         const verification = await twilio.verifications.create({
